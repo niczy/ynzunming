52195cdb8639bba4922eb29c266b40106e44ab6e
Nicholas <nicholas.zhaoyu@gmail.com>
2012-03-09 03:58:52 -0500

    MOD: add bootstrap.

--- server.py
+++ server.py
@@ -6,10 +6,14 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render('index.html')
 
+class FeedHandler(web.RequestHandler):
+    def get(self):
+        self.render('feed.html')
+
 application = tornado.web.Application([
-    (r"/", MainHandler),
-    (r"/static/(.*)", web.StaticFileHandler, {"path": "./static/"})
-], template_path = "./template")
+	(r"/", MainHandler),
+	(r"/static/(.*)", web.StaticFileHandler, {"path": "./static/"})
+], template_path = "./template", debug=True)
 
 print application.settings.get("template_path")
 
